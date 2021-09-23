e683ea2c4bf2e3083b3b6d55f3e05dac8ce2022e
YusrilIsmailAzi <60691798+Yusrilismailazi@users.noreply.github.com>
2021-09-23 18:19:38 +0700

    Menyelesaikan Video 7 Constant

---
+++ constant.php
@@ -0,0 +1,30 @@
+<?php
+
+// define('NAMA', 'Aji');
+
+// echo NAMA;
+// echo"<br>";
+
+// const UMUR = 50;
+// echo UMUR;
+
+
+// class Coba {
+//     const NAMA = 'Aji';
+// }
+
+// echo Coba::NAMA;
+
+// echo __FILE__;
+
+// function coba(){
+//     return __FUNCTION__;
+// }
+// echo coba();
+
+class Coba{
+    public $kelas = __CLASS__;
+}
+$obj = new Coba;
+echo $obj->kelas;
+?>
